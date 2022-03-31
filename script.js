dcd896e234a1ef3458bb3b842d3dcd7e0b900f38
Ariane Morelato <aniemorelanie@gmail.com>
2022-03-31 13:10:46 -0300

    js

--- script.js
+++ script.js
@@ -4,14 +4,16 @@ const inputsCriarConta = document.querySelectorAll('#criar .ct-input input')
 const alertas = document.querySelectorAll('.alert')
 const submitBtn = document.querySelector('#submit-conta')
 const inputSenhaRpt = document.querySelector('#senharpt')
-
+const termosInput = document.querySelector('#termos')
+const btnDarkmode = document.querySelector('.lightdarkmode')
+let inputField
 
 //A senha deve possuir, ao menos, 10 caracteres. Letras maiusculas e minusculas (obrigatorio), caracteres especiais (@:;<.>/*-+_+(*%¨&*()||\'"!@#$$%"), e umnumero, 
 const regex = {
     usuario: /^\w{5,}/,
     email: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
     senha: /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/,
-    senharpt: null
+    senharpt: undefined
 }
 
 allInputs.forEach((item) => {
@@ -41,18 +43,19 @@ btns.forEach((item, index) => {
 })
 
 
-function validateInput(event, index) {
+function contemClasse(classe) {
+    return inputField.classList.contains(classe);
+}
 
-    const { value, id } = event.target;
-    const inputField = event.target.parentElement;
+function eliminarClasse(classe) {
+    inputField.classList.remove(classe);
+}    
 
-    function contemClasse(classe) {
-        return inputField.classList.contains(classe);
-    }
 
-    function eliminarClasse(classe) {
-        inputField.classList.remove(classe);
-    }    
+function validateInput(input, index) {
+
+    const { value, id } = input;
+    inputField = input.parentElement;
 
     if (contemClasse('invalido')) eliminarClasse('invalido');
     if (contemClasse('valido')) eliminarClasse('valido');
@@ -67,13 +70,38 @@ function validateInput(event, index) {
         }
     }
 
-    if (id === 'senha') regex.senharpt = value;
+    if (id === 'senha') {
+        regex.senharpt = new RegExp('^' + value + '$');
+        validateInput(inputSenhaRpt, 3)
+    }
 }
 
-inputsCriarConta.forEach((input, index) => input.addEventListener('change', (event) => validateInput(event, index)))
+inputsCriarConta.forEach((input, index) => 
+input.addEventListener('change', (event) => validateInput(event.target, index))
+)
 
-function submitForm() {
-    
+function submitForm(event) {
+
+    inputsCriarConta.forEach((input,index) => validateInput(input, index));
+
+    const arrayInputs = Array.from(inputsCriarConta)
+
+    function isInvalid(element) {
+        return element.parentElement.classList.contains('invalido')
+    }
+
+    if (arrayInputs.some(isInvalid) || !termosInput.checked) {
+        event.preventDefault()
+    }
 }
 
 submitBtn.addEventListener('click', submitForm)
+
+
+// lightmode e darkmode 
+
+function changeMode() {
+    document.body.classList.toggle('darkmode');
+}
+
+btnDarkmode.addEventListener('click', changeMode)
